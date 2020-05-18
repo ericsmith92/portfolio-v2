633b40cb2c123eeeee37ebfce2fef6656ad8e4f5
ericsmith92 <ericsmth884@gmail.com>
2020-05-18 14:12:14 -0400

    first commit with some setup

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header'
+import Banner from './Banner';
+import Footer from './Footer';
+import '../styles.scss';
+
+const App = () => {
+    return(
+        <div>
+            <Header />
+            <Banner />
+            <Footer />
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Banner extends React.Component{
+
+    render(){
+        return(
+            <div className="banner">
+                
+            </div>
+        )
+    }
+}
+
+export default Banner;
