6c9b88ce44c2c44bcc26536f649fbd6866b08304
ericsmith92 <ericsmth884@gmail.com>
2020-05-29 08:16:49 -0400

    switch toggle to button to avoid keyboard trap

--- src/components/Card/CardToggle.js
+++ src/components/Card/CardToggle.js
@@ -5,11 +5,11 @@ class CardToggle extends React.Component{
     render(){
         const { toggleCardState } = this.props;
         return(
-            <div className='card_toggle' onClick={() => toggleCardState()}>
+            <button className='card_toggle' onClick={() => toggleCardState()}>
                 <span></span>
                 <span></span>
                 <span></span>
-            </div>
+            </button>
         )
     }
 }
