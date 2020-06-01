45823873ef83ebff9e72f27a7f35fc49657b13b4
ericsmith92 <ericsmth884@gmail.com>
2020-05-31 20:39:43 -0400

    few more style tweaks

--- src/components/Card/CardReveal.js
+++ src/components/Card/CardReveal.js
@@ -3,13 +3,13 @@ import Highlighter from "react-highlight-words";
 
 const CardReveal = ({ toggleCardState, accomplishments,  githubURL, projectURL }) => {
     const keyWords = ['Node', 'Node JS', 'JavaScript', 'JAMstack', 'React', 'stitching'];
-    const accomplishmentsList = accomplishments.map((text, index) => <li key={index}><Highlighter
-    highlightClassName="card_reveal__mark"
-    searchWords={keyWords}
-    autoEscape={true}
-    textToHighlight={text}
-/></li>);
-    //TODO: make a new component and wraps keywords in <strong></strong> tags
+    const accomplishmentsList = accomplishments.map((text, index) => <li key={index}>
+                                                                        <Highlighter
+                                                                            highlightClassName="card_reveal__mark"
+                                                                            searchWords={keyWords}
+                                                                            autoEscape={true}
+                                                                            textToHighlight={text}
+                                                                    /></li>);
     return(
        <div className='card_reveal'>
            <div className='card_reveal__heading'>
