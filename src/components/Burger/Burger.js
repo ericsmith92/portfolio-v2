2e89d114a6eb9f4e45f0ce7b8519ebf01af7f5e8
ericsmith92 <ericsmth884@gmail.com>
2020-06-03 07:38:16 -0400

    a few small tweaks

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Burger = ({open , setOpen}) => {
     return(
-        <div className={`burger ${open ? 'open' : ''}`}  open={open} onClick={() => setOpen(!open)}>
+        <button className={`burger ${open ? 'open' : ''}`}  open={open} onClick={() => setOpen(!open)}>
             <span></span>
             <span></span>
             <span></span>
-        </div>
+        </button>
     )
 }
 
