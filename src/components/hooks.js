cb453893ffd651669e4bc7e826d65724483ce928
ericsmith92 <ericsmth884@gmail.com>
2020-06-10 08:28:16 -0400

    switch from overflow-y hidden to overflow hidden because safari sucks

--- src/components/hooks.js
+++ src/components/hooks.js
@@ -21,10 +21,10 @@ export const useLockBodyScroll = (open) => {
   useLayoutEffect(() => {
    if(open){
     const scrollBarWidth = window.innerWidth - document.querySelector('body').offsetWidth;
-    const originalStyle = window.getComputedStyle(document.body).overflowY; 
+    const originalStyle = window.getComputedStyle(document.body).overflow; 
     document.body.style.paddingRight = scrollBarWidth + 'px';
-    document.body.style.overflowY = 'hidden';
-   return () => [document.body.style.overflowY = originalStyle, document.body.style.paddingRight = '0px' ];
+    document.body.style.overflow = 'hidden';
+   return () => [document.body.style.overflow = originalStyle, document.body.style.paddingRight = '0px' ];
    }
    }); 
 }
