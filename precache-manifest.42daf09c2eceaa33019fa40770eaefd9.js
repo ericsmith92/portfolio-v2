b63d0339812b507eafe7cbf8dbf52bb88f3aa86b
ericsmith92 <ericsmth884@gmail.com>
2020-06-13 11:46:22 -0400

    Updates

---
+++ precache-manifest.42daf09c2eceaa33019fa40770eaefd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6089773aa99cc395bbcd4e5f3dc70361",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dffb23872b1cb0ac196c",
+    "url": "/static/css/main.7eb857c6.chunk.css"
+  },
+  {
+    "revision": "b4a364258d50fa927274",
+    "url": "/static/js/2.904c2b56.chunk.js"
+  },
+  {
+    "revision": "40158217dcba29c41bf6c1afe3c208d6",
+    "url": "/static/js/2.904c2b56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dffb23872b1cb0ac196c",
+    "url": "/static/js/main.e4725c85.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  }
+]);
