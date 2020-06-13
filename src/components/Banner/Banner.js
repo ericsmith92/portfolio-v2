a6261bbcf92246ab72748e346cffb231135d1ba0
ericsmith92 <ericsmth884@gmail.com>
2020-06-13 10:07:14 -0400

    loader added while awaitin fetch

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,6 +2,7 @@ import React from 'react';
 import BannerImg from './BannerImg';
 import BannerBio from './BannerBio';
 import BannerAnchor from './BannerAchor';
+import BannerLoader from './BannerLoader';
 
 class Banner extends React.Component{
 
@@ -20,15 +21,25 @@ class Banner extends React.Component{
     }
 
     render(){
-        return(
-            <div className="banner">
-                <div className="banner_inner">
-                    {this.state.me ? <BannerImg src={this.state.me.avatar_url}/> : ''}
-                    {this.state.me ? <BannerBio text={this.state.me.bio}/> : ''}
-                    <BannerAnchor dataValue='work'/>
+        if(Object.keys(this.state.me).length){
+            return(
+                <div className="banner">
+                    <div className="banner_inner">
+                        {this.state.me ? <BannerImg src={this.state.me.avatar_url}/> : ''}
+                        {this.state.me ? <BannerBio text={this.state.me.bio}/> : ''}
+                        <BannerAnchor dataValue='work'/>
+                    </div>
                 </div>
-            </div>
-        )
+            )
+        }else{
+            return(
+                <div className="banner">
+                    <div className="banner_inner">
+                        <BannerLoader />
+                    </div>
+                </div>
+            )
+        }
     }
 }
 
