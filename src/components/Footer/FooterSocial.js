30da5a36cfb0b3af495ced95ce7d8e327599cb6a
ericsmith92 <ericsmth884@gmail.com>
2020-12-30 14:07:04 -0500

    target blank added to footer links

--- src/components/Footer/FooterSocial.js
+++ src/components/Footer/FooterSocial.js
@@ -4,17 +4,17 @@ const FooterSocial = () => {
     return(
         <ul className="footer_social">
             <li>
-                <a href="https://github.com/ericsmith92">
+                <a target="_blank" rel="noopener noreferrer" href="https://github.com/ericsmith92">
                     <i className="fa fa-github brand-icon"></i>
                 </a>
             </li>
             <li>
-                <a href="https://www.linkedin.com/in/smitheric92/">
+                <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/smitheric92/">
                     <i className="fa fa-linkedin-square brand-icon"></i>
                 </a>
             </li>
             <li>
-                <a href="http://helloericsmith.com/resume.pdf">
+                <a target="_blank" rel="noopener noreferrer" href="http://helloericsmith.com/resume.pdf">
                     <i className="fa fa-file-text" aria-hidden="true"></i>
                 </a>
             </li>
